b92bd9bfd803706f38c024444b2954142fead69f
Rajesh Budhathoki <rajoyish@gmail.com>
2023-02-26 23:00:28 +0545

    03. Enum methods

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -7,4 +7,13 @@ enum OrderStatus: String
     case PLACED = 'placed';
     case SHIPPED = 'shipped';
     case CANCELLED = 'cancelled';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::PLACED => 'Placed',
+            self::SHIPPED => 'Shipped',
+            self::CANCELLED => 'Cancelled',
+        };
+    }
 }
--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -18,7 +18,7 @@
                             <select id="status" name="status"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                 @foreach ($statuses as $status)
-                                    <option value="{{ $status }}">{{ $status }}</option>
+                                    <option value="{{ $status }}">{{ $status->label() }}</option>
                                 @endforeach
                             </select>
                         </div>
