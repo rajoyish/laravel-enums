ac30bab8b61a6571d457c5a619aa78c906d6e26c
Rajesh Budhathoki <rajoyish@gmail.com>
2023-03-06 20:17:56 +0545

    Using accessors to return enums

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +12,11 @@ class Order extends Model
     use HasFactory;
 
     protected $fillable = ['status'];
+
+    protected function status(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => OrderStatus::tryFrom($value)
+        );
+    }
 }
--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -20,7 +20,7 @@
                             <select id="status" name="status"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                 @foreach ($statuses as $status)
-                                    <option value="{{ $status }}" @selected($order->status === $status->value)>
+                                    <option value="{{ $status }}" @selected($order->status === $status)>
                                         {{ $status->label() }}</option>
                                 @endforeach
                             </select>
